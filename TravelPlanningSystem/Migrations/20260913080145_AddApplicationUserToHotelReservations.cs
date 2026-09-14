fdab8f8237abceff9558fdb7abde997af4370ff5
Blanken22 <168002155+Blanken22@users.noreply.github.com>
2026-09-14 15:27:47 +0800

    database fix

--- TravelPlanningSystem/Migrations/20260913080145_AddApplicationUserToHotelReservations.cs
+++ TravelPlanningSystem/Migrations/20260913080145_AddApplicationUserToHotelReservations.cs
@@ -11,54 +11,39 @@ namespace TravelPlanningSystem.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "DisruptionMessage",
-                table: "ActivityBookings");
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[HotelReservations]') AND name = 'ApplicationUserId')
+                BEGIN
+                    ALTER TABLE [dbo].[HotelReservations] ADD [ApplicationUserId] uniqueidentifier NULL;
+                END
 
-            migrationBuilder.AddColumn<Guid>(
-                name: "ApplicationUserId",
-                table: "HotelReservations",
-                type: "uniqueidentifier",
-                nullable: true);
+                IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Flights]') AND name = 'DiscountPercent')
+                BEGIN
+                    DECLARE @var nvarchar(max);
+                    SELECT @var = QUOTENAME([d].[name])
+                    FROM [sys].[default_constraints] [d]
+                    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+                    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Flights]') AND [c].[name] = N'DiscountPercent');
+                    IF @var IS NOT NULL EXEC(N'ALTER TABLE [Flights] DROP CONSTRAINT ' + @var + ';');
+                    ALTER TABLE [Flights] ALTER COLUMN [DiscountPercent] decimal(18,2) NOT NULL;
+                END
 
-            migrationBuilder.AlterColumn<decimal>(
-                name: "DiscountPercent",
-                table: "Flights",
-                type: "decimal(18,2)",
-                nullable: false,
-                oldClrType: typeof(decimal),
-                oldType: "decimal(5,2)");
+                IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[FlightBookings]') AND name = 'DisruptionMessage')
+                BEGIN
+                    ALTER TABLE [dbo].[FlightBookings] ADD [DisruptionMessage] nvarchar(500) NULL;
+                END
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "EstimatedArrivalTime",
-                table: "Flights",
-                type: "datetime2",
-                nullable: true);
+                IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_HotelReservations_ApplicationUserId' AND object_id = OBJECT_ID(N'[dbo].[HotelReservations]'))
+                BEGIN
+                    CREATE NONCLUSTERED INDEX [IX_HotelReservations_ApplicationUserId] ON [dbo].[HotelReservations] ([ApplicationUserId]);
+                END
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "EstimatedDepartureTime",
-                table: "Flights",
-                type: "datetime2",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "DisruptionMessage",
-                table: "FlightBookings",
-                type: "nvarchar(500)",
-                maxLength: 500,
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_HotelReservations_ApplicationUserId",
-                table: "HotelReservations",
-                column: "ApplicationUserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_HotelReservations_Users_ApplicationUserId",
-                table: "HotelReservations",
-                column: "ApplicationUserId",
-                principalTable: "Users",
-                principalColumn: "UserId");
+                IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = N'FK_HotelReservations_Users_ApplicationUserId')
+                BEGIN
+                    ALTER TABLE [dbo].[HotelReservations] WITH CHECK ADD CONSTRAINT [FK_HotelReservations_Users_ApplicationUserId] 
+                    FOREIGN KEY([ApplicationUserId]) REFERENCES [dbo].[Users] ([UserId]);
+                END
+            ");
         }
 
         /// <inheritdoc />
@@ -77,31 +62,8 @@ namespace TravelPlanningSystem.Migrations
                 table: "HotelReservations");
 
             migrationBuilder.DropColumn(
-                name: "EstimatedArrivalTime",
-                table: "Flights");
-
-            migrationBuilder.DropColumn(
-                name: "EstimatedDepartureTime",
-                table: "Flights");
-
-            migrationBuilder.DropColumn(
                 name: "DisruptionMessage",
                 table: "FlightBookings");
-
-            migrationBuilder.AlterColumn<decimal>(
-                name: "DiscountPercent",
-                table: "Flights",
-                type: "decimal(5,2)",
-                nullable: false,
-                oldClrType: typeof(decimal),
-                oldType: "decimal(18,2)");
-
-            migrationBuilder.AddColumn<string>(
-                name: "DisruptionMessage",
-                table: "ActivityBookings",
-                type: "nvarchar(500)",
-                maxLength: 500,
-                nullable: true);
         }
     }
 }
--- TravelPlanningSystem/Migrations/20260913083205_AddStaffUserToHotelReservations.cs
+++ TravelPlanningSystem/Migrations/20260913083205_AddStaffUserToHotelReservations.cs
@@ -9,25 +9,40 @@ namespace TravelPlanningSystem.Migrations
     public partial class AddStaffUserToHotelReservations : Migration
     {
         /// <inheritdoc />
+//       protected override void Up(MigrationBuilder migrationBuilder)
+//       {
+//           migrationBuilder.AddColumn<Guid>(
+//               name: "StaffUserId",
+//               table: "HotelReservations",
+//               type: "uniqueidentifier",
+//               nullable: true);
+//
+//           migrationBuilder.CreateIndex(
+//               name: "IX_HotelReservations_StaffUserId",
+//               table: "HotelReservations",
+//               column: "StaffUserId");
+//
+//           migrationBuilder.AddForeignKey(
+//               name: "FK_HotelReservations_StaffUsers_StaffUserId",
+//               table: "HotelReservations",
+//               column: "StaffUserId",
+//               principalTable: "StaffUsers",
+//               principalColumn: "StaffId");
+//       }
+
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<Guid>(
-                name: "StaffUserId",
-                table: "HotelReservations",
-                type: "uniqueidentifier",
-                nullable: true);
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[HotelReservations]') AND name = 'StaffUserId')
+                BEGIN
+                    ALTER TABLE [dbo].[HotelReservations] ADD [StaffUserId] uniqueidentifier NULL;
+                END
 
-            migrationBuilder.CreateIndex(
-                name: "IX_HotelReservations_StaffUserId",
-                table: "HotelReservations",
-                column: "StaffUserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_HotelReservations_StaffUsers_StaffUserId",
-                table: "HotelReservations",
-                column: "StaffUserId",
-                principalTable: "StaffUsers",
-                principalColumn: "StaffId");
+                IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_HotelReservations_StaffUserId' AND object_id = OBJECT_ID(N'[dbo].[HotelReservations]'))
+                BEGIN
+                    CREATE NONCLUSTERED INDEX [IX_HotelReservations_StaffUserId] ON [dbo].[HotelReservations] ([StaffUserId]);
+                END
+            ");
         }
 
         /// <inheritdoc />
